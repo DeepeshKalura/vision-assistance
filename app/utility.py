e0d92b97fafd84f89d0f502b27f7edf0dff834b8
DeepeshKalura <deepeshkalurs@gmail.com>
2024-05-09 02:22:56 +0530

    Add utility functions for IoT image processing and location retrieval

--- app/utility.py
+++ app/utility.py
@@ -1,6 +1,10 @@
-from functools import wraps
 import time
-
+from functools import wraps
+import cv2
+import requests
+import numpy as np
+import geocoder
+import webbrowser
 #? cet = calculate_execution_time
 
 
@@ -24,3 +28,36 @@ def cet(func):
     print(f"Execution time of {func.__name__}: {execution_time:.3f} seconds")
     return result
   return wrapper
+
+
+def iot_to_cv_server():
+  url = "http://192.168.164.45/"
+  jpeg_stream = requests.get(url, stream=True)
+
+  if jpeg_stream.status_code == 200:
+    bytes_data = bytes()
+    for chunk in jpeg_stream.iter_content(chunk_size=1024):
+        bytes_data += chunk
+        a = bytes_data.find(b'\xff\xd8')
+        b = bytes_data.find(b'\xff\xd9')
+        if a != -1 and b != -1:
+            jpg = bytes_data[a:b+2]
+            bytes_data = bytes_data[b+2:]
+            frame = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+            if frame is not None:
+                    yield frame
+  else:
+      raise ConnectionError(f"Failed to connect to the server with status code: {jpeg_stream.status_code}")
+
+
+
+def location_with_ip_address():
+    g = geocoder.ip('me')
+    print(g.latlng)
+    location = g.address
+    print(location)
+
+  
+
+def location_with_gps():
+  pass
