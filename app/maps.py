9d95bb4d87c4732f61cedb4fdb7ad597802543c8
Pranjal Rana <129268721+PRANJALRANA11@users.noreply.github.com>
2024-05-09 02:02:54 +0530

    adding-sos

---
+++ app/maps.py
@@ -0,0 +1,27 @@
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key='AIzaSyBuXRk4iP-jBj_KTUBJv7r_BT7mk0_wGcU')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
+
+# Validate an address with address validation
+addressvalidation_result =  gmaps.addressvalidation(['1600 Amphitheatre Pk'], 
+                                                    regionCode='US',
+                                                    locality='Mountain View', 
+                                                    enableUspsCass=True)
+# print(addressvalidation_result)
+print(directions_result)
+# print(reverse_geocode_result)
+# print(geocode_result)
