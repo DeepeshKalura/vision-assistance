962198aa35d4284a0622f89341447d85e546da05
Varun Chopra <varunchopra2003@gmail.com>
2024-05-09 06:07:44 +0530

    Width Detection Added

---
+++ model_data/widther/main.py
@@ -0,0 +1,18 @@
+from Detector import *
+import os
+
+def main():
+    server_address = "http://192.168.137.188/"
+    configPath = os.path.join("model_data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
+    modelPath = os.path.join("model_data", "frozen_inference_graph.pb")
+    classesPath = os.path.join("model_data", "coco.names")
+
+    detector = Detector(server_address, configPath, modelPath, classesPath)
+    detector.start_processing()
+
+    input("Press Enter to stop processing...")
+
+    detector.stop_processing()
+
+if __name__ =='__main__':
+    main()
