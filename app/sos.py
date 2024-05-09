dc7409ce4856ea827fa4ac26ee8fe1f30f310ba2
DeepeshKalura <deepeshkalurs@gmail.com>
2024-05-09 06:14:46 +0530

    added excectable time decorater

--- app/sos.py
+++ app/sos.py
@@ -3,9 +3,7 @@ import os
 from twilio.rest import Client
 from dotenv import load_dotenv
 
-
-from app.utility import location_with_ip_address
-
+from app.utility import cet, location_with_ip_address
 load_dotenv()
 
 # Find your Account SID and Auth Token at twilio.com/console
@@ -14,7 +12,7 @@ account_sid = os.getenv("ACCOUNT_SID")
 auth_token = os.getenv("AUTH_TOKEN")
 
 client = Client(account_sid, auth_token)
-
+@cet
 def help_sms()->bool:
     location, lantlang = location_with_ip_address()
     sms = f"Alert! Deepesh Kalura need urgent help, his location is {location} and have latitude and longitude {lantlang}"
