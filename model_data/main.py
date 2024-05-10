0227e7ddffa934fb9e7be1039801450bada2a929
Varun Chopra <varunchopra2003@gmail.com>
2024-05-10 15:45:44 +0530

    main changes

--- model_data/main.py
+++ model_data/main.py
@@ -2,8 +2,7 @@ from Detector import *
 import os
 
 def main():
-    server_address = "http://192.168.1.1"
-
+    server_address = "http://192.168.1.1/"
     configPath = os.path.join("model_data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
     modelPath = os.path.join("model_data", "frozen_inference_graph.pb")
     classesPath = os.path.join("model_data", "coco.names")
