3dfd575697a9f36f04916509542b3f0d904d9bc4
DeepeshKalura <deepeshkalurs@gmail.com>
2024-05-11 00:59:49 +0530

    added detection to the front-end

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -3,8 +3,12 @@ import "regenerator-runtime/runtime";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
 export default function Home() {
+  const [button_status, setButtonStatus] = useState(true);
   const {
     transcript,
     listening,
@@ -14,34 +18,25 @@ export default function Home() {
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
+  
   useEffect(() => {
     if (transcript.includes("describe")) {
-      console.log("You are right");
+  
+      
     }
     if (transcript.includes("help")) {
       console.log("You are right1");
     }
-    if (transcript.includes("great")) {
-      console.log("You are right2");
-    }
   }, [transcript]);
+  useEffect(() => {
+    SpeechRecognition.startListening({ continuous: true });
+  },[])
+  const notify = () => toast("Wow so easy!");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <video class="w-[40rem] rounded-lg" autoPlay controls>
-        <source src="video4.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div>
-        <p>Microphone: {listening ? "on" : "off"}</p>
-        <button
-          onClick={SpeechRecognition.startListening({ continuous: true })}
-        >
-          Start
-        </button>
-        <button onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
-        <p>{transcript}</p>
-      </div>
+      <img class="w-[40rem] rounded-lg" src="http://127.0.0.1:8000/stream/"/>
+    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" >Start</button>
     </main>
+        
   );
 }
