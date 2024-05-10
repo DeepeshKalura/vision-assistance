e7cc0ac77dac8d6eedd13fccff53ec4ce8449c6a
DeepeshKalura <deepeshkalurs@gmail.com>
2024-05-11 02:43:38 +0530

    refactor the server.py that not functionality of the application

--- server.py
+++ server.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
-import requests
 
-from model_data.improved_detector import process_frames, receive_frames 
 from app import multimodel, sos, read, obstacle_detection
 
 app = FastAPI(title="Video Assistance API", version="0.0.1")
